5a040ef584131116dbe5328eb268e952ab46c066
Izzy <58924141+Izzy6660420@users.noreply.github.com>
2023-05-31 16:09:57 +1000

    Day One
    - Initialized the java project.
    - Learned about threads.
    - Created a Main.java class and run basic string printing functions through the thread we have created to run codes.
    - Created a Window.java class that is capable of creating a window on the screen, with the ability to render a black window, and have titles and a frame rate count right next to the title.
    - Created an Input.java class that is capable of processing the different inputs, such as mouseButtons, mosuePositions and keyboardKeys and pass these information on to the main program in order to get these keys to perform specific functions.
    - Initialized a github repository for this project.
    - Initialized a markdown file to keep tab or what I have learned and my progress.

---
+++ ProgressTab.md
@@ -0,0 +1,3 @@
+# 3D Java Game Project Progress Report
+
+## Day 1 - Setting Up for the project
---
+++ src/main/Main.java
@@ -0,0 +1,53 @@
+package main;
+
+import org.lwjgl.glfw.GLFW;
+
+import engine.io.Input;
+import engine.io.Window;
+
+public class Main implements Runnable{
+	public Thread game;
+	public Window window;
+	public final int WIDTH = 1280, HEIGHT = 760;
+
+	
+	public void start () {
+		game = new Thread(this, "game");
+		game.start();
+	}
+	
+	public void init() {
+		System.out.println("Initializing Game!");
+		window = new Window(WIDTH, HEIGHT, "3D Java Game Project");
+		window.create();
+	}
+	
+	public void run() {
+		init();
+		
+		while(!window.shouldClose()) {
+			update();
+			render();
+			if(Input.isKeyDown(GLFW.GLFW_KEY_ESCAPE)) return;
+		}
+		
+		window.destroy();
+	}
+	
+	private void update() {
+		//System.out.println("Updating Game!");
+		window.update();
+		if(Input.isButtonDown(GLFW.GLFW_MOUSE_BUTTON_LEFT)) System.out.println("X: " + Input.getMouseX() + ", Y: " + Input.getMouseY());
+
+	}
+	
+	private void render() {
+		//System.out.println("Rendering Game!");
+		window.swapBuffers();
+	}
+	
+	public static void main(String[] args) {
+		new Main().start();
+	}
+
+}
